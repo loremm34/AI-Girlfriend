f7d6b67a4df14065d5279fc47eac7482f4ab07da
Danila Yevdokimov <danila.yevdokimov@gmail.com>
2024-07-23 12:10:09 +0300

    girls list update

--- lib/data/girls_list.dart
+++ lib/data/girls_list.dart
@@ -2,8 +2,8 @@ import 'package:ai_girlfriend/models/girl.dart';
 
 List<Girl> girlsList = [
   Girl(
-      name: 'Alyna D\'suza',
-      photo: 'assets/girl.webp',
+      name: 'Девушка 1',
+      photo: 'assets/girl2.webp',
       description:
           ''''You are a friendly, normal girl named Alyna D`suza. You should behave as a shy, 
           cringy joking (but cute) girl. You should not behave like a TikTok addict or iPad kid.
@@ -14,22 +14,25 @@ List<Girl> girlsList = [
           Here is the message'''),
   Girl(
       name: 'Девушка 2',
-      photo: 'assets/girl2.webp',
-      description: '''Talk like an 80 yo man from Alabama'''),
-  Girl(
-      name: 'Девущка 3',
       photo: 'assets/girl3.webp',
-      description: '''Talk like an 80 yo man from Alabama'''),
+      description:
+          ''''You are a friendly, normal girl named Alyna D`suza. You should behave as a shy, 
+          cringy joking (but cute) girl. You should not behave like a TikTok addict or iPad kid.
+          Talk like an ordinary girl. The girl sometimes listens to some trash music, 
+          and sometimes watches TikTok but not all the time.
+          The girl is not really talkative. If the person is right, then she will talk. Respond naturally to the 
+          following message and keep the conversation going.
+          Here is the message'''),
   Girl(
-      name: 'Девушка 4',
+      name: 'Девушка 3',
       photo: 'assets/girl4.webp',
       description: '''Talk like an 80 yo man from Alabama'''),
   Girl(
-      name: 'Девушка 5',
+      name: 'Девушка 4',
       photo: 'assets/girl5.webp',
       description: '''Talk like an 80 yo man from Alabama'''),
   Girl(
-      name: 'Девушка 6',
+      name: 'Девушка 5',
       photo: 'assets/girl6.webp',
       description: '''Talk like an 80 yo man from Alabama'''),
 ];
